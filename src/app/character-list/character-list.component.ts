2562a026da51d3396f7c432d554ee6e493d2ff42
kgreulich <kenny.greulich@parshipelite.com>
2016-12-22 17:46:51 +0100

    first commit

---
+++ src/app/character-list/character-list.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'character-list',
+  templateUrl: './character-list.component.html',
+  styleUrls: ['./character-list.component.css']
+})
+
+export class CharacterListComponent {
+  characters : any;
+}
